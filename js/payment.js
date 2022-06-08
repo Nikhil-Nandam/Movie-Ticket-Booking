bf306145a4b942b36cce6aec78f721bf4efdbf39
Nikhil Nandam <nikhil.nandam@gmail.com>
2022-06-08 18:19:26 +0530

    add netbanking functionality to payments page

--- js/payment.js
+++ js/payment.js
@@ -1,10 +1,12 @@
 const contact=document.getElementById("wrapper");
 let contactShow=true;
-
+const netBanking=document.getElementById("net-banking");
+let showNetBanking=true;
 function showContact(){
     if ( contactShow ){
         contact.hidden=false;
         contactShow=false;
+
     }
     else{
         contact.hidden=true;
@@ -12,3 +14,16 @@ function showContact(){
     }
 
 }
+function showNetBank(){
+    if ( showNetBanking ){
+        netBanking.hidden=false;
+        showNetBanking=false;
+
+    }
+    else{
+        netBanking.hidden=true;
+        showNetBanking =true
+    }
+
+}
+
