905df2de9a97dc2457866cead22ed46fb7081bd2
Nikhil Nandam <nikhil.nandam@gmail.com>
2022-06-27 18:02:06 +0530

    make changes corresponding to directory structure change for js files

--- js/payment.js
+++ js/payment.js
@@ -1,7 +1,12 @@
 const contact=document.getElementById("wrapper");
 let contactShow=true;
+const payment=document.getElementById("payment");
+let paymentShow=true;
 const netBanking=document.getElementById("net-banking");
-let showNetBanking=true;
+const UPI=document.getElementById("UPI");
+const wallet=document.getElementById("wallet");
+const credit_debit=document.getElementById("credit-debit");
+
 function showContact(){
     if ( contactShow ){
         contact.hidden=false;
@@ -14,16 +19,39 @@ function showContact(){
     }
 
 }
-function showNetBank(){
-    if ( showNetBanking ){
-        netBanking.hidden=false;
-        showNetBanking=false;
-
+function showPayment(){
+    if ( paymentShow ){
+        payment.hidden=false;
+        paymentShow=false;
+        showNetBank();
     }
     else{
-        netBanking.hidden=true;
-        showNetBanking =true
+        payment.hidden=true;
+        paymentShow =true
     }
 
 }
-
+function showNetBank(){
+    netBanking.hidden=false;
+    UPI.hidden=true;
+    wallet.hidden=true;
+    credit_debit.hidden=true;
+}
+function showUpi(){
+    netBanking.hidden=true;
+    UPI.hidden=false;
+    wallet.hidden=true;
+    credit_debit.hidden=true;
+}
+function showWallet(){
+    netBanking.hidden=true;
+    UPI.hidden=true;
+    wallet.hidden=false;
+    credit_debit.hidden=true;
+}
+function showCreditDebit(){
+    netBanking.hidden=true;
+    UPI.hidden=true;
+    wallet.hidden=true;
+    credit_debit.hidden=false;
+}
